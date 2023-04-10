3242ab133e9c5ef4b9f358aad9b6ace67d47903e
limaico <francisco.limaico@kushkipagos.com>
2023-04-10 16:09:54 -0500

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Francisco Limaico on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
